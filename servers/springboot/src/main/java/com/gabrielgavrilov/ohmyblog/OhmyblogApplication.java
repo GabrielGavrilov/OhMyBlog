f0f69fa79c1e174de10b27d237e22fb287b385cb
Gabriel Gavrilov <gabriel.gavrilov02@gmail.com>
2025-07-19 11:52:36 -0600

    update

---
+++ servers/springboot/src/main/java/com/gabrielgavrilov/ohmyblog/OhmyblogApplication.java
@@ -0,0 +1,13 @@
+package com.gabrielgavrilov.ohmyblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OhmyblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OhmyblogApplication.class, args);
+	}
+
+}
---
+++ servers/springboot/src/test/java/com/gabrielgavrilov/ohmyblog/OhmyblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrielgavrilov.ohmyblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OhmyblogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
